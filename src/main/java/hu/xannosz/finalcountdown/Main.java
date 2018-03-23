9dc296d46c31ae1fd75f7130f667ffaff7d9ea5a
Xannosz <joli.neni@freemail.hu>
2018-03-23 17:24:27 +0100

    Last commit

--- src/main/java/hu/xannosz/finalcountdown/Main.java
+++ src/main/java/hu/xannosz/finalcountdown/Main.java
@@ -1,10 +1,60 @@
 package hu.xannosz.finalcountdown;
 
-public class Main {
-    
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
 
+import javax.swing.JLabel;
+import javax.swing.JWindow;
 
-    public static void main(String[] args) {
+public class Main extends JWindow {
 
-    }
+	private static final long serialVersionUID = -7718123547252332352L;
+	private Date time;
+	private JLabel panel = new JLabel();
+	private SimpleDateFormat sdf;
+
+	private Main(String str) throws ParseException {
+
+		sdf = new SimpleDateFormat("MM/dd/yyyy");
+		time = sdf.parse(str);
+
+		setVisible(true);
+		setAlwaysOnTop(true);
+		setSize(new Dimension(100, 40));
+		setLayout(new GridLayout(1, 1));
+
+		panel.setForeground(Color.GREEN);
+		getContentPane().setBackground(Color.BLACK);
+
+		setLocation(0, 730);
+
+		add(panel);
+	}
+
+	public static void main(String[] args) {
+		Main main;
+		try {
+			main = new Main("04/30/2018");
+			while (true) {
+				main.refresh();
+				Thread.sleep(5L);
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	private void refresh() throws ParseException {
+		long now = Instant.now().toEpochMilli();
+
+		long diffInMillies = Math.abs(now - time.getTime());
+
+		panel.setText("    " + diffInMillies);
+		setVisible(true);
+	}
 }
